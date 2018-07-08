6aca8d84d793b61efe4809b586b67fd9e2f4600c
Alex Holder <alex.holder@bigfootds.com>
2018-07-08 21:58:15 +1000

    Tweaked the folder structure generator and added a "actor camera"-type function to the toolkit.

--- Assets/BigfootDS/_Common/Scripts/FolderStructureGenerator.cs
+++ Assets/BigfootDS/_Common/Scripts/FolderStructureGenerator.cs
@@ -33,12 +33,21 @@ public class FolderStructureGenerator : MonoBehaviour {
 public class FolderStructureWizard : ScriptableWizard {
     // Using a ScriptableWizard creates a pop-up Unity tab, allowing us to customize the functions we call before we run them.
     // Read more about it here: https://docs.unity3d.com/ScriptReference/ScriptableWizard.html
-
+    [Tooltip("Make all folders listed below inside this folder. Basically setting a master or parent folder for the generator. Leave blank to let it generate directly into the root of your Assets folder.")]
+    public string foldersPrefix = "";
+    [Tooltip("One folder per entry in the list. You can make quick paths by using slashes - such as folder1/folder2 to make a folder containing a folder.")]
     public string[] customFoldersToGenerate = new string[] { "__Sandbox", "_Demo", "_ImportedAssets", "Animations", "Audio", "Fonts", "Materials", "Models", "Prefabs", "Plugins", "ScriptableObjects", "Scripts", "Shaders", "Sprites", "Textures", "Audio/SFX", "Audio/Music", "Scripts/Editor", "Plugins/iOS", "Plugins/Android" };
+    // Bigfoot's favourite folder structure:
+    // "__Sandbox", "_Demo", "_ImportedAssets", "Animations", "Audio", "Fonts", "Materials", "Models", "Prefabs", "Plugins", "ScriptableObjects", "Scripts", "Shaders", "Sprites", "Textures", "Audio/SFX", "Audio/Music", "Scripts/Editor", "Plugins/iOS", "Plugins/Android"
+
+    // Privately handle backing up the default list. Devs can change the default list of folders coded up above, and their changes are safely respected by the folder generation wizard's "Reset" button.
+    [HideInInspector] public string[] backupFoldersToGenerate = new string[0];
+    bool hasBackedUpList = false;
 
     [MenuItem("BigfootDS/Custom Generate Folders in Project")]
     static void CreateFolderStructureWizard()
     {
+        
         ScriptableWizard.DisplayWizard<FolderStructureWizard>("Folder Structure Wizard", "Create Folders", "Reset Options");
         
     }
@@ -47,6 +56,13 @@ public class FolderStructureWizard : ScriptableWizard {
 
     void OnWizardUpdate()
     {
+        if (!hasBackedUpList)
+        {
+            // Back up the default list of folders to generate.
+            // This allows other devs to change the hardcoded list, and let the rest of the script automatically integrate it into the backups system.
+            hasBackedUpList = true;
+            backupFoldersToGenerate = customFoldersToGenerate;
+        }
         helpString = "Please set the folders that you would like to generate!";
         // helpString is automatically picked up by the ScriptableWizard type.
     }
@@ -56,11 +72,17 @@ public class FolderStructureWizard : ScriptableWizard {
     void OnWizardCreate()
     {
         Debug.Log("Starting the custom project folder structure generator via the wizard.");
+
+        string folderPrefix = "Assets/";
+        if (foldersPrefix.Length > 0)
+        {
+            folderPrefix += "/" + foldersPrefix + "/";
+        }
         for (int i = 0; i < customFoldersToGenerate.Length; i++)
         {
             // The "CreateDirectory" function automatically does nothing if the filepath is invalid or already used.
             // So no if statements or crazy validation checking needed - we can just run the function!
-            Directory.CreateDirectory("Assets/" + customFoldersToGenerate[i]);
+            Directory.CreateDirectory(folderPrefix + customFoldersToGenerate[i]);
         }
 
         // Forcing an asset database refresh means we will see these new folders in our Project tab instantly.
@@ -72,6 +94,7 @@ public class FolderStructureWizard : ScriptableWizard {
     // Think of it as the secondary button of the wizard.
     void OnWizardOtherButton()
     {
-        customFoldersToGenerate = new string[] { "_Demo", "_ImportedAssets", "Animations", "Audio", "Fonts", "Materials", "Models", "Prefabs", "ScriptableObjects", "Scripts", "Shaders", "Sprites", "Textures", "Audio/SFX", "Audio/Music", "Scripts/Editor" };
+        customFoldersToGenerate = backupFoldersToGenerate;
+        //customFoldersToGenerate = new string[] { "_Demo", "_ImportedAssets", "Animations", "Audio", "Fonts", "Materials", "Models", "Prefabs", "ScriptableObjects", "Scripts", "Shaders", "Sprites", "Textures", "Audio/SFX", "Audio/Music", "Scripts/Editor" };
     }
 }
---
+++ Assets/BigfootDS/_Common/Scripts/SceneViewActorCamera.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class SceneViewActorCamera : MonoBehaviour {
+
+
+    [MenuItem("BigfootDS/Move Main Camera To SceneView Camera")]
+    public static void QuickMoveMainCamToSceneCam()
+    {
+        // This will grab the first camera tagged as "Main Camera" in the Hierarchy and make its transform values match the Scene View camera's transform values. It'll be a mess if you have more than one camera tagged as "Main Camera"!
+        Camera.main.transform.position = SceneView.lastActiveSceneView.camera.transform.position;
+        Camera.main.transform.rotation = SceneView.lastActiveSceneView.camera.transform.rotation;
+        Camera.main.transform.localScale = SceneView.lastActiveSceneView.camera.transform.localScale;
+        Debug.Log("Moved the scene's Main Camera to match the position, rotation and scale of the Scene View camera!");
+        
+    }
+
+
+}
