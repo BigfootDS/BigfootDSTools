b92af2722283c4e784fba7188f7149b23c27d2b6
Alex Holder <alex.holder@bigfootds.com>
2018-07-31 20:58:58 +1000

    Added a custom event invoker/component.

---
+++ Assets/BigfootDS/_Common/Scripts/BigfootEventInvoker.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using BigfootDS;
+
+namespace BigfootDS
+{
+
+    public class BigfootEventInvoker : MonoBehaviour
+    {
+        [System.Serializable]
+        public class SpecifiedEvent : UnityEvent { }
+
+        public SpecifiedEvent eventAction = new SpecifiedEvent();
+
+        public void StartEventAction()
+        {
+            eventAction.Invoke();
+        }
+
+
+    }
+}
